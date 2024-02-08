bd15a94e451a4cdc8499275780adc9117c926997
babanoelk <124783309+babanoelk@users.noreply.github.com>
2024-02-08 14:57:21 +0100

    .

--- BackEndExercisesByAhmad/src/main/java/week2/fridayReview/Main.java
+++ BackEndExercisesByAhmad/src/main/java/week2/fridayReview/Main.java
@@ -1,26 +1,49 @@
 package week2.fridayReview;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 // Understand what JSON is and how to use Jackson or Gson to convert between JSON and DTOs
 public class Main {
     public static void main(String[] args) {
 
+        ExecutorService worker = Executors.newFixedThreadPool(2);
+
         // Example 1
         /*
-        Example1 example1 = new Example1();
+        Runnable example1 = new Example1();
 
-        ExecutorService worker = Executors.newCachedThreadPool();
         worker.execute(example1);
 
         System.out.println("Hovedtråden kører.");
 
         worker.shutdown();
-
-         */
+        /*
 
         // Example 2
+        /*
+        Callable<Integer> eksempel2 = new Example2();
+
+        Future<Integer> futureResultat = worker.submit(eksempel2);
+
+        // Future
+        //  1) Future kommer fra Javas Future API.
+        //  2) Man får et fremtidigt result, som man kan arbejde med, når resultatet er klar.
+        //  3) .get metoden venter på at resultatet er færdigt, så man kan arbejde videre med det.
+
+
+        System.out.println("Hovedtråden kører.");
+
+        try {
+            Integer resultat = futureResultat.get();
+            System.out.println("Resultatet fra opgaven er: " + resultat);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        worker.shutdown();
+        */
     }
 }
 
@@ -37,9 +60,13 @@ class Example1 implements Runnable {
 }
 
 
-class Example2 implements Callable<T> {
-    @Override
-    public T call() throws Exception {
-        return T;
+// Formålet:
+// En opgave, der kan køres i en separat tråd, der kan udføres samtidigt med andre opgaver.
+
+// Metode:
+// Simulerer en opgave, der returnerer en værdi
+class Example2 implements Callable<Integer> {
+    public Integer call() throws Exception {
+        return 42;
     }
 }
