ab7e84814483170dc00919ec62da73fc124f4f4a
babanoelk <124783309+babanoelk@users.noreply.github.com>
2024-02-09 10:01:17 +0100

    .

--- BackEndExercisesByAhmad/src/main/java/week2/fridayReview/Main.java
+++ BackEndExercisesByAhmad/src/main/java/week2/fridayReview/Main.java
@@ -1,72 +1,26 @@
 package week2.fridayReview;
 
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
-// Understand what JSON is and how to use Jackson or Gson to convert between JSON and DTOs
+
+// Know the purpose of Runnable and Callable interfaces plus Future
+
 public class Main {
     public static void main(String[] args) {
-
-        ExecutorService worker = Executors.newFixedThreadPool(2);
-
-        // Example 1
-        /*
-        Runnable example1 = new Example1();
-
-        worker.execute(example1);
-
-        System.out.println("Hovedtråden kører.");
-
-        worker.shutdown();
-        /*
-
-        // Example 2
-        /*
-        Callable<Integer> eksempel2 = new Example2();
-
-        Future<Integer> futureResultat = worker.submit(eksempel2);
-
-        // Future
-        //  1) Future kommer fra Javas Future API.
-        //  2) Man får et fremtidigt result, som man kan arbejde med, når resultatet er klar.
-        //  3) .get metoden venter på at resultatet er færdigt, så man kan arbejde videre med det.
-
-
-        System.out.println("Hovedtråden kører.");
-
-        try {
-            Integer resultat = futureResultat.get();
-            System.out.println("Resultatet fra opgaven er: " + resultat);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        worker.shutdown();
-        */
     }
 }
 
+// Formålet med både Runnable og Callable
 
-// Formålet:
-// En opgave, der kan køres i en separat tråd, der kan udføres samtidigt med andre opgaver.
-
-// Metode:
-// Interfacet har en enkelt metode kaldet run(), hvor du kan skrive koden til opgaven.
 class Example1 implements Runnable {
+    @Override
     public void run() {
-        System.out.println("En anden tråd kører.");
     }
 }
 
-
-// Formålet:
-// En opgave, der kan køres i en separat tråd, der kan udføres samtidigt med andre opgaver.
-
-// Metode:
-// Simulerer en opgave, der returnerer en værdi
-class Example2 implements Callable<Integer> {
-    public Integer call() throws Exception {
-        return 42;
+class Example2 implements Callable<String> {
+    @Override
+    public String call() throws Exception {
+        return null;
     }
 }
